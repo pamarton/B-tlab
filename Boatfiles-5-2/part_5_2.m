19cd08bf458657a20144e49548b320d1361d3611
Tor Erik Larsen <torlarse@ntnu.no>
2017-11-10 13:35:50 +0100

    jobber med 5.d
    Signed-off-by: Tor Erik Larsen <torlarse@ntnu.no>

--- Boatfiles-5-2/part_5_2.m
+++ Boatfiles-5-2/part_5_2.m
@@ -14,12 +14,44 @@ nfft = [];
 load('wave.mat')
 
 %% Estimating Power Spectral Density PSD
-[pxx,f] = pwelch(psi_w(2,:).*pi/180,window,noverlap,nfft,fs);
+[S_psi,f] = pwelch(psi_w(2,:).*pi/180,window,noverlap,nfft,fs);
 % Scaling power per Hz to power s/rad
-pxx = pxx*1/(2*pi);
+S_psi = S_psi*1/(2*pi);
 % Scaling frequency to rad/s
 omega = f*2*pi;
+% Eigen frequency from plot
+omega_0 = 0.7823;
+% Max amplitude from spectral plot
+sigma_squared = 1.5E-03;
+% Standard deviation from spectral plot
+sigma = sqrt(sigma_squared);
+%% Identifying damping factor
+%lambda = 1;
+%omega_vector = 1:150;
+%Kw = 2*lambda*omega_0*sigma;
 
-%% Plotting psi as in part 5.2.c
-plot(pxx(1:150,1))
-% TODO: add annotation
+%% Plotting estimated PSD in part 5.2.c
+figure
+hold on
+plot(omega, S_psi)
+xlim([0,1.5])
+%xlim([0,1.5])
+xlabel('Angular frequency, rad')
+ylabel('Estimated Spectral Density')
+
+%% Curve fitting
+P_psi = @(lambda,omega)((2*lambda*omega_0*sigma)^2.*omega.^2) ./...
+    ((omega_0^2 - omega.^2) + (2*lambda*omega_0^2.*omega.^2));
+%/ ((omega_0^2 - omega_vector.^2) +...
+ %   (2*lambda*omega_0^2.*omega_vector.^2));
+x0 = 0.1;
+lambda_fitted = lsqcurvefit(P_psi,x0,omega,S_psi);
+%% Plotting curve fitting in part 5.2.d
+figure
+hold on
+plot(omega, S_psi)
+xlim([0,1.5])
+plot(omega, P_psi(lambda_fitted,omega))
+%xlim([0,1.5])
+xlabel('Angular frequency, rad')
+ylabel('Estimated Spectral Density')
