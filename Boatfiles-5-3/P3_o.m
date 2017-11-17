0c74f08f4a4e42d71ebfcce54f13199265820b87
Per Amund Roaulsson Marton <pamarton@ntnu.no>
2017-11-17 14:08:42 +0100

    Signed-off-by: Per Amund Roaulsson Marton <pamarton@ntnu.no>

--- Boatfiles-5-3/P3_o.m
+++ Boatfiles-5-3/P3_o.m
@@ -1,20 +1,35 @@
-A_1_top = 63.34;
-A_1_bot = 4.64;
-A_2_top = 4.23;
-A_2_bot = 2.57;
-A_1 = (A_1_top - A_1_bot)/2;
-A_2 = (A_2_top - A_2_bot)/2;
-w_1 = 0.005;
-w_2 = 0.05;
+clear all
+clc
 
-T_d = ((A_2^2 * w_2^2 - A_1^2 * w_1^2)/(A_1^2 * w_1^4 - A_2^2 * w_2^4))^(1/2);
-K = A_1 * (w_1^4 * T^2 + w_1^2)^(1/2);
-
-
+addpath('../Data/');
+load('var_o1b.mat');
+%load('var_o2');
 
 w_c = 0.1;
 phase_margin = 50;
 deg2rad = pi/180;
 rad2deg = 180/pi;
 T_f = -1/(w_c* tan((180-phase_margin)*deg2rad));
-K_pd = sqrt(T_f^2 * w_c^4 + w_c^2)/K
+K_pd = sqrt(T_f^2 * w_c^4 + w_c^2)/K;
+T_d  = T;
+course_angle = 30;
+course_angle_error = 35;
+
+hold on
+addpath('b/');
+load_system('op5_3b.slx');
+sim('op5_3b.slx');
+plot(s_xy(:,1),s_xy(:,2));
+save('../Data/var_o3b.mat','w_c','T_f','T_d','K_pd','s_t','s_compass','s_xy','s_delta','s_course_angle','s_course_angle_ref');
+
+addpath('c/');
+load_system('op5_3c.slx');
+sim('op5_3c.slx');
+plot(s_xy(:,1),s_xy(:,2));
+save('../Data/var_o3c.mat','w_c','T_f','T_d','K_pd','s_t','s_compass','s_xy','s_delta','s_course_angle','s_course_angle_ref');
+
+addpath('d/');
+load_system('op5_3d.slx');
+sim('op5_3d.slx');
+plot(s_xy(:,1),s_xy(:,2));
+save('../Data/var_o3d.mat','w_c','T_f','T_d','K_pd','s_t','s_compass','s_xy','s_delta','s_course_angle','s_course_angle_ref');
