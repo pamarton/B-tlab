97e3e4c18331fd3d15bdaaec8cf11d6c99163db7
Tor Erik Larsen <torlarse@ntnu.no>
2017-11-17 10:51:57 +0100

    added initial data in 5.4.c
    Signed-off-by: Tor Erik Larsen <torlarse@ntnu.no>

--- Boatfiles-5-5/part_5_5.m
+++ Boatfiles-5-5/part_5_5.m
@@ -4,3 +4,12 @@
 [Ad, Ed] = c2d(A, E, Ts);
 Cd = C;
 Dd = 0;
+
+%% Matrices given in part 5.5.c
+% Initial a priori estimate error covariance
+P_0_minus = [1 0 0 0 0; 0 0.013 0 0 0; 0 0 pi^2 0 0; 0 0 0 1 0;
+    0 0 0 0 2.5e-03];
+% Process noise covariance
+Q = [30 0; 0 1e-06];
+% Initial a priori state estimate
+x_0_minus = zeros(5,1);
